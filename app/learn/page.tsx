9803161d5296d24211391fbd71c77864b90b97df
AndrewJunisay <2413018@STUDENTS.UNIVERSITASMULIA.AC.ID>
2026-04-23 12:12:44 +0800

    second commit

---
+++ app/learn/page.tsx
@@ -0,0 +1,4 @@
+export default function learnpage (){
+    return <p>ini adalah halaman learn!</p>;
+}
+ 
--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            hallo Andrew 
           </a>
         </div>
       </main>
