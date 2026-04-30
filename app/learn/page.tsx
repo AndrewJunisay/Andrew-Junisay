46665c5de7f37e04e1eea8c8da9d062389082447
AndrewJunisay <2413018@STUDENTS.UNIVERSITASMULIA.AC.ID>
2026-04-30 12:18:33 +0800

    commit lagi

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,4 +1,17 @@
-export default function learnpage (){
-    return <p>ini adalah halaman learn!</p>;
+export default function learnpage() {
+    return (
+        <div className="bg-purple-300"> 
+        <div className="bg-orange-300"> 
+            <img /> 
+            <p>design</p>
+            </div> 
+            <div> 
+                <h1>Figma</h1>
+                <p> 8 Hours Ago</p>
+            </div>
+        </div>
+        
+    )     
+
 }
  
